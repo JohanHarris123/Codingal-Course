28b16f667c24df2b2dc0d520e0707eb2b15384b4
JohanHarris123 <yashboss791@gmail.com>
2025-08-19 19:54:08 +0800

    Initial Commit

---
+++ M8/Classwork Activity/L2_B.py
@@ -0,0 +1,10 @@
+setx = {"green", "blue", "yellow"}
+sety = {"blue", "blue", "yellow"}
+seta = {"red", "green", "blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print(seta)
+print("\nIntersection of three said sets:")
+setz = sety.intersection(setx, seta)
+print(setz)
---
+++ M8/Classwork Activity/L2_D.py
@@ -0,0 +1,10 @@
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+setc3 = {"red", "green", "yellow"}
+print("Original sets: ")
+print(setc1)
+print(setc2)
+print(setc3)
+setc = setc1.union(setc2, setc3)
+print("\nUnion of the sets: ")
+print(setc)
