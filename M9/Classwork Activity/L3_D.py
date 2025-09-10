f0ef45db7e1e6b0661cbdb50a38a8759c2a668e0
JohanHarris123 <yashboss791@gmail.com>
2025-09-10 19:33:28 +0800

    Initial Commit

---
+++ M9/Classwork Activity/L3_D.py
@@ -0,0 +1,11 @@
+with open('Codingal.txt') as fp:
+    data1 = fp.read()
+
+with open('Codingal Classes.txt') as fp:
+    data2 = fp.read()
+
+data1 += "\n"
+data1 += data2
+print("Merging two files....")
+with open('MergedFile.txt', 'w') as fp:
+    fp.write(data1)
