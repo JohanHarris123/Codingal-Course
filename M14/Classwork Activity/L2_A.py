ad5b495b19f7efc4849c644e85abe4f8a33496d1
JohanHarris123 <yashboss791@gmail.com>
2025-12-23 19:37:15 +0800

    Initial Commit

---
+++ M14/Classwork Activity/L2_A.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+mydataset = {
+    'cars': ['BMW', 'Mercedes', 'Tesla'],
+    'passings': [3, 7, 4]
+}
+
+df1 = pd.DataFrame(mydataset)
+print(df1)
+df = pd.DataFrame(mydataset, index=[1,2,3])
+print(df)
+
+print(df.iloc[0,[0,1]]) #index
+print(df.loc[1,['cars']]) #label
+
+a = [1,2,3]
+ser1 = pd.Series(a)
+print(ser1)
+ser = pd.Series([1,2,3], index=['a', 'b', 'c'])
+print(ser)
+ser2 = pd.Series({"day1": 520, "day2": 390, "day3": 230})
+print(ser2)
+
+df = pd.read_csv("Countries Data.csv")
+print(df.head())
+print(df.info())
+print("is null:", df.isnull().sum())
+
+#df.dropna(inplace=True)
+#print(df.tail())
+
+#df.fillna(1300, inplace=True)
+#print(df.tail())
